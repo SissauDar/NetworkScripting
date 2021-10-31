2b513c58c11f9e446022929eb32e2491decf80aa
Dar Sissau <dar.sissau@student.howest.be>
2021-10-31 17:42:19 +0100

    ex2dc2

---
+++ Powershell/Ex02/DC2/01_hiddenshare.ps1
@@ -0,0 +1,13 @@
+﻿$dir = "c:\profiles"
+New-Item -Path $dir -type directory -Force
+New-SmbShare -Name 'profiles$' -Path $dir -FullAccess "Everyone"
+
+
+$acl = Get-Acl $dir
+$acl.SetAccessRuleProtection($True, $False)
+$acl.Access | % { $acl.RemoveAccessRule($_) }
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule('administrators', 'FullControl', "ContainerInherit, objectInherit", "None", 'Allow')
+$acl.AddAccessRule($rule)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule('Authenticated Users', 'Modify', "None", "None", "Allow")
+$acl.AddAccessRule($rule)
+(Get-Item $profilesFolder).SetAccessControl($acl)
